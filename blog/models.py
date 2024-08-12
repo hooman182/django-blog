ad6567f446cee5e73318e1c4c8ef2e40975b0091
hooman182 <hoomanm87@gmail.com>
2024-08-12 22:10:00 +0330

    feat: Add pagination, slug to url, pagination

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from pyexpat import model
 from django.db import models
 from django.db.models.query import QuerySet
@@ -17,7 +18,7 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish', allow_unicode=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(timezone.now)
@@ -35,3 +36,7 @@ class Post(models.Model):
 
     def __str__(self) -> str:
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail", args=[self.publish.year, self.publish.month, self.publish.day, self.slug])
+    
--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,17 @@
-from django.http import Http404
+from django.views.generic import ListView
 from django.shortcuts import get_object_or_404, render
 from blog.models import Post
+from django.core.paginator import Paginator, EmptyPage
 
 # Create your views here.
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'posts.html', {'posts': posts})
 
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 2
+    template_name = 'posts.html'
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status = Post.Status.PUBLISHED)
+def post_detail(request, year, month, day, post_slug):
+    post = get_object_or_404(Post, status = Post.Status.PUBLISHED, publish__year=year, publish__month=month, publish__day=day, slug=post_slug)
+    
     return render(request, 'postDetail.html', {'post': post})
