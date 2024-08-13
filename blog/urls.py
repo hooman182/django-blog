b7540ecd588351a9e2dd89992fa7786c5b877cd9
hooman182 <hoomanm87@gmail.com>
2024-08-13 23:56:59 +0330

    feat: Creat Comment model, view and form

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:year>/<int:month>/<int:day>/<str:post_slug>', views.post_detail, name='post_detail')
+    path('<int:year>/<int:month>/<int:day>/<str:post_slug>',
+         views.post_detail, name='post_detail'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.views.generic import ListView
-from django.shortcuts import get_object_or_404, render
-from blog.models import Post
+from django.shortcuts import get_object_or_404, redirect, render
+from blog.forms import CommentForm
+from blog.models import Comment, Post
 from django.core.paginator import Paginator, EmptyPage
-
+from django.views.decorators.http import require_POST
 # Create your views here.
 
 class PostListView(ListView):
@@ -14,4 +15,21 @@ class PostListView(ListView):
 def post_detail(request, year, month, day, post_slug):
     post = get_object_or_404(Post, status = Post.Status.PUBLISHED, publish__year=year, publish__month=month, publish__day=day, slug=post_slug)
     
-    return render(request, 'postDetail.html', {'post': post})
+    comments = Comment.objects.filter(active=True, post=post.id)
+    form = CommentForm()
+    
+    return render(request, 'postDetail.html', {'post': post, 'comments':comments, 'form':form})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    comment = None
+    
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+        
+    return render(request, 'comment.html', {'post': post, 'comment':comment})
